16f11353495a8b20607b72769d63680ed0772981
Marius Pop <marius@mlpinit.com>
2013-06-25 15:38:08 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookmarker::Application.config.secret_key_base = '1390e3a7322d5faff15bb47c71e2f061b0a08e02aa00375b744609268ac8b6e545077a9aabe5cc3876da1dc38ea04042468055ec25011ea26b407aa9cff51a97'
