93858d3ae1fc6099a895b098896b4fb4038652ea
Marius Pop <marius@mlpinit.com>
2013-07-01 21:16:45 -0400

    configured pg

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '4.0.0.rc1'
 gem 'bcrypt-ruby', :require => 'bcrypt'
 
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'pg'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0.rc1'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130630134308) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookmarks", force: true do |t|
     t.string   "path"
     t.integer  "site_id"
