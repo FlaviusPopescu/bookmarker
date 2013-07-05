26f6859a2e27ff9a0e1d427e1435fb8a206a8eb5
Marius Pop <marius@mlpinit.com>
2013-07-05 10:54:15 -0400

    commented unfinished spec

--- spec/features/new_bookmark_spec.rb
+++ spec/features/new_bookmark_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'Bookmark creation' do
 
-  scenario 'user creates new bookmark' do
+  xscenario 'user creates new bookmark' do
     sign_in
     visit new_bookmark_path
     fill_in :new_bookmark, with: 'http://morningstar.com'
