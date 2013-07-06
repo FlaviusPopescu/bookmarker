56050784006492485c4159afa2ad869e52cbb0a2
Marius Pop <marius@mlpinit.com>
2013-07-06 07:59:29 -0400

    fixed spec

--- spec/features/new_bookmark_spec.rb
+++ spec/features/new_bookmark_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 feature 'Bookmark creation' do
 
-  xscenario 'user creates new bookmark' do
+  scenario 'user creates new bookmark' do
     sign_in
     visit new_bookmark_path
     fill_in :new_bookmark, with: 'http://morningstar.com'
-    click_button 'Create Bookmark'
+    click_button 'Create'
 
     expect(page).to have_content('morningstar.com')
   end
